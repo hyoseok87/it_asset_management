845201d4c6a631e58ecb63cd1dd329f5e2fa2110
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-05-23 12:21:57 +0200

    tutorial

--- asset_management/forms.py
+++ asset_management/forms.py
@@ -1,8 +1,17 @@
 # asset_management/forms.py
 from django import forms
 from .models import Asset
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class AssetForm(forms.ModelForm):
     class Meta:
         model = Asset
         fields = ['name', 'asset_type', 'location', 'purchase_date', 'price']
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
--- asset_management/urls.py
+++ asset_management/urls.py
@@ -3,10 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('assets/', views.asset_list, name='asset_list'),
-    path('assets/<int:asset_id>/', views.asset_detail, name='asset_detail'),
-    path('assets/new/', views.asset_create, name='asset_create'),
-    path('assets/<int:asset_id>/edit/', views.asset_edit, name='asset_edit'),
-    path('assets/<int:asset_id>/delete/', views.asset_delete, name='asset_delete'),
+    path('', views.asset_list, name='asset_list'),
+    path('<int:asset_id>/', views.asset_detail, name='asset_detail'),
+    path('add/', views.asset_create, name='asset_create'),
+    path('<int:asset_id>/edit/', views.asset_edit, name='asset_edit'),
+    path('<int:asset_id>/delete/', views.asset_delete, name='asset_delete'),
 ]
